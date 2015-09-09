23d3479eb496660b3e168acab9c13bb8123448c9
aslabde <aslabde@gmail.com>
2015-09-10 00:58:23 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name unameWebAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the unameWebAppApp
+ */
+angular.module('unameWebAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name unameWebAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the unameWebAppApp
+ */
+angular.module('unameWebAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
